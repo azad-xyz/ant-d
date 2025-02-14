0aeb1ad28006b9c8a4df85555be7217f6e6dbf09
azad-xyz <jrazad10@gmail.com>
2025-02-14 17:44:09 +0600

    feat: implement token-based authentication with protected routes and redux-persist

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,33 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { verifyToken } from "../utils/verifyToken";
 
 const Login = () => {
+  const dispatch = useAppDispatch();
   const { register, handleSubmit } = useForm({
     defaultValues: {
       userId: "A-0001",
       password: "admin123",
     },
   });
-  const [login, { data, error }] = useLoginMutation();
+  const [login, { error }] = useLoginMutation();
 
-  console.log("data=>", data);
-  console.log("error=>", error);
+  // console.log("data=>", data);
+  // console.log("error=>", error);
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     // console.log(data);
     const userInfo = {
       id: data.userId,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+
+    const user = verifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
 
   return (
